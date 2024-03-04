ce1e469fb7dda1f3014b8ab41765124ffb897759
JCSweet <45698957+JCSweet@users.noreply.github.com>
2024-03-04 14:04:42 -0700

    Initial Commit - full project files
    Initial commit. Fully functioning project files. All my original code. Built as Capstone Project for part of Angela Yu's Web Dev bootcamp. Project was to build a blog web app using Node backend and EJS. Feature requirements were ability for user to Create, Edit, and Delete blog posts. I added "like" and comment features as well. Design is minimal and needs improvement but it's fully functioning and works in mobile / desktop. Currently requires server to be hosted locally.

---
+++ index.js
@@ -0,0 +1,143 @@
+import express from "express";
+import { v4 as uuidv4 } from "uuid";
+
+const app = express();
+const port = 3000;
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.render("index.ejs", { blogPost: blogPosts });
+});
+
+function newBlogPost(req) {
+  const addPost = {
+    id: uuidv4(),
+    author: req.author,
+    content: req.content,
+    likes: 1,
+    comments: [],
+  };
+  blogPosts.push(addPost);
+//   console.log(blogPosts);
+}
+
+function newBlogPostComment(req) {
+  console.log(req);
+  const postID = req.postID;
+  const addComment = {
+    commentID: uuidv4(),
+    commentAuthor: req.commentAuthor,
+    commentContent: req.commentContent,
+  };
+  const idx = blogPosts.findIndex((item) => item.id === postID);
+  blogPosts[idx].comments.push(addComment);
+//   console.log(blogPosts[idx].comments);
+}
+
+function addLike(postID) {
+  const idx = blogPosts.findIndex((item) => item.id === postID);
+  blogPosts[idx].likes += 1;
+}
+
+function deletePost(postID) {
+  const idx = blogPosts.findIndex((item) => item.id === postID);
+  blogPosts.splice(idx, 1);
+//   console.log(`Deleting index: ${idx}`);
+}
+
+function deleteComment(postID, cID) {
+  const pIdx = blogPosts.findIndex((item) => item.id === postID);
+  const cIdx = blogPosts[pIdx].comments.findIndex((c) => c.commentID === cID);
+  blogPosts[pIdx].comments.splice(cIdx, 1);
+//   console.log(`Deleting post index: ${pIdx} -> comment index: ${cIdx}`);
+}
+
+function editPost(postID, editedPost) {
+  const idx = blogPosts.findIndex((item) => item.id === postID);
+  blogPosts[idx].content = editedPost;
+}
+
+app.post("/submit", (req, res) => {
+  newBlogPost(req.body);
+  res.redirect("/");
+});
+
+app.post("/submitComment", (req, res) => {
+  newBlogPostComment(req.body);
+  res.redirect("/");
+});
+
+app.post("/edit", (req, res) => {
+  // EDIT
+  const postID = req.body.postID;
+  const editedPost = req.body.editedPost;
+  //   console.log(editedPost);
+  editPost(postID, editedPost);
+  res.redirect("/");
+});
+
+app.post("/delete", (req, res) => {
+  // DELETE
+  const postID = req.body.postID;
+  // console.log(postID);
+  deletePost(postID);
+  res.redirect("/");
+});
+
+app.post("/deleteComment", (req, res) => {
+  // DELETE
+  const postID = req.body.postID;
+  const commentID = req.body.commentID;
+  // console.log(req.body);
+  deleteComment(postID, commentID);
+  res.redirect("/");
+});
+
+app.post("/addLike", (req, res) => {
+  // EDIT
+  const postID = req.body.postID;
+  addLike(postID);
+  res.redirect("/");
+});
+
+app.listen(port, (req, res) => {
+  console.log(`Captain! Servers engaged on port ${port}`);
+});
+
+const blogPosts = [
+  {
+    id: uuidv4(),
+    author: "Jordan",
+    content: "hello thar mateys!",
+    likes: 1,
+    comments: [
+      {
+        commentID: uuidv4(),
+        commentAuthor: "Rocky",
+        commentContent: "Maow 😸",
+      },
+      {
+        commentID: uuidv4(),
+        commentAuthor: "Tigie",
+        commentContent: "Weeeeeowwww 🙀",
+      },
+    ],
+  },
+  {
+    id: uuidv4(),
+    author: "Jordan",
+    content:
+      "who can tell me the best way to clean kitty 🤮 out of the carpet?",
+    likes: 3,
+    comments: [
+      {
+        commentID: uuidv4(),
+        commentAuthor: "Emily",
+        commentContent: "😬",
+      },
+    ],
+  },
+];
