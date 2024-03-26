4ebf004a932212265e4fff4d5eea6fa9ad211429
JCSweet <45698957+JCSweet@users.noreply.github.com>
2024-03-26 10:51:24 -0600

    add railway variable for port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from "express";
 import { v4 as uuidv4 } from "uuid";
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT;
 
 app.use(express.urlencoded({ extended: true }));
 
